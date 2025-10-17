e460e430150a068425f3c1b4d0c6b0db7337e195
mojo <ardhiant.r.a@gmail.com>
2025-10-17 08:20:02 +0700

    commit d1f3e2a7f8c9b6e5a4b3c2d1e0f9a8b7c6d5e4f3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,55 @@
 
+let tasksDB = [];
 
-function addtask(){ }
+function addtask() { 
+    const taskinput = document.getElementById("todo-input");
+    const taskdate = document.getElementById("due-date");
 
-function deletetask(){ }
+    console.log(taskinput.value);
+    console.log(taskdate.value);
 
-function filtertask(){ }
+    if (validateinput(taskinput.value, taskdate.value )) {
+        const newtask={
+            task: taskinput.value,
+            date: taskdate.value,
+        }
 
-function validateinput(){ }
+        tasksDB.push(newtask);
+
+        renderTask();
+    } 
+}
+
+function renderTask() { const tasklist = document.getElementById("task-list");
+    tasklist.innerHTML = "";
+
+    tasksDB.forEach((task, index)=> {
+        tasklist.innerHTML += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${task.task}</td>
+            <td>${task.date}</td>
+            <td><button onclick="deletetask(${index})">Delete</button></td>
+        </tr>
+        `;
+    });
+}
+
+function deleteAllTasks(){
+    tasksDB = [];
+    renderTask();
+
+}
+
+function filtertask(){
+    const filterdate = document.getElementById("filter-task").value;
+
+}
+
+function validateinput(task, date){
+    if (task.trim() === "" || date.trim() === "") {
+        alert("Please enter both task and due date.");
+        return false;
+    }
+    return true;
+}
